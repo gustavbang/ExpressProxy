7ae29864040319f6c50df02549fdefb36d35181a
Gustav Bang <gustavbang96@gmail.com>
2020-01-13 13:56:56 +0100

    Realtime Data

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var port = 8000;
 
 var server = "52.58.159.100";
 
-
 app.use(require("body-parser").json());
 
 app.use(express.static("public"));
@@ -34,12 +33,33 @@ app.post("/express_backend", function(req, res) {
 
   rp(options)
       .then(function(body) {
-        console.log("sending body")
-        console.log(body)
         res.send(body)
       })
       .catch(err => res.send(err));
 });
+
+app.post("/realtime_data", function(req, res) {
+  console.log(req.body.plantid)
+  console.log(req.body.openApiroarand)
+  var options = {
+    method: "POST",
+    uri: `https://${server}:27200/openApi/queryDeviceDetail`,
+    form: {
+      plantid: req.body.plantid,
+      openApiroarand: req.body.openApiroarand
+    },
+    headers: {},
+    insecure: true,
+    rejectUnauthorized: false,
+  }
+
+  rp(options)
+      .then(function(body) {
+        res.send(body)
+      })
+      .catch(err => res.send(err));
+});
+
 
 
 
